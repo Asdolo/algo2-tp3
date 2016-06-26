9a95a5372a34159e6c353b32f02ec5b073af418a
fedealliani <fedealliani@gmail.com>
2016-06-26 17:28:47 -0300

    Creacion BaseDeDatos.h
    Saque los '?' de Dato.h y comence a completar BaseDeDatos.h

--- Dato.h
+++ Dato.h
@@ -5,11 +5,11 @@
 #include "TiposBasicos.h"
 
     class Dato{
-        bool mismoTipo?(Dato d1,Dato d2);
+        bool mismoTipo(Dato d1,Dato d2);
         Dato min(Conj(Dato) c);
         Dato max(Conj(Dato) c)
         bool menorOIgual(Dato d1,Dato d2);
-        bool sonIguales?(Dato d1,Dato d2);
+        bool sonIguales(Dato d1,Dato d2);
 
         public:
 
@@ -26,19 +26,19 @@
             /**
              * Devuelve true si el dato es de tipo nat.
              */
-            bool esNat?();
+            bool esNat();
 
             unsigned int valorNat();
 
             const string& valorStr();
 
-            bool esString?();
+            bool esString();
 
             Dato(const Dato& otro);
 
 
         private:
-            bool nat?;
+            bool nat;
             String valorStr;
             unsigned int valorNat;
     };
@@ -50,39 +50,39 @@
     std::ostream& operator<<(std::ostream& os, const Dato&);
 
 
-Dato::datoNat(unsigned int n) : nat?(true), valorStr(""),valorNat(n) {}
+Dato::datoNat(unsigned int n) : nat(true), valorStr(""),valorNat(n) {}
 
-Dato::datoString(String s) : nat?(false), valorStr(s),valorNat(0) {}
-Dato::Dato(const Dato& otro) : nat?(otro.nat?),valorStr(otro.valorStr),valorNat(otro.valorNat){}
+Dato::datoString(String s) : nat(false), valorStr(s),valorNat(0) {}
+Dato::Dato(const Dato& otro) : nat(otro.nat),valorStr(otro.valorStr),valorNat(otro.valorNat){}
 
 
-bool Dato::esNat?(){
-	return this.nat?;
+bool Dato::esNat(){
+	return this.nat;
 }
 
 unsigned int Dato::valorNat(){
-	assert(nat?);
+	assert(nat);
 	return this.valorNat;
 }
 
 const string& Dato::valorStr(){
-	assert(!nat?);
+	assert(!nat);
 	return this.valorStr;
 }
 
-bool Dato::esString?(){
-	return !(this.nat?);
+bool Dato::esString(){
+	return !(this.nat);
 }
 
 
-    bool mismoTipo?(Dato d1,Dato d2){
-    	return (d1.esNat?==d2.esNat?);
+    bool mismoTipo(Dato d1,Dato d2){
+    	return (d1.esNat==d2.esNat);
     }
 
     Dato min(Conj(Dato) c){
     	Iterador it = c.crearIt();
     	Dato minimo = it.siguiente();
-    	while(it.haySiguiente?()){
+    	while(it.haySiguiente()){
     		if ( menorOIgual(it.siguiente(),minimo)){
     			minimo=it.siguiente();
     		}
@@ -94,7 +94,7 @@ bool Dato::esString?(){
       Dato max(Conj(Dato) c){
     	Iterador it = c.crearIt();
     	Dato maximo = it.siguiente();
-    	while(it.haySiguiente?()){
+    	while(it.haySiguiente()){
     		if ( !menorOIgual(it.siguiente(),maximo)){
     			maximo=it.siguiente();
     		}
@@ -104,8 +104,8 @@ bool Dato::esString?(){
     }
 
 bool menorOIgual(Dato d1, Dato d2){
-	assert(mismoTipo?(d1,d2))
-	if (d1.esNat?){
+	assert(mismoTipo(d1,d2))
+	if (d1.esNat){
 		return d1.datoNat<=d2.datoNat;
 	}
 	else{
@@ -122,8 +122,8 @@ bool menorOIgual(Dato d1, Dato d2){
 }
 
     bool operator==(const Dato& d1, const Dato d2&){
-    	if (mismoTipo?(d1,d2)){
-    		if (d1.esNat?){
+    	if (mismoTipo(d1,d2)){
+    		if (d1.esNat){
     			return (d1.valorNat==d2.valorNat);
     		}else{
     			return (d1.valorStr==d2.valorStr);
