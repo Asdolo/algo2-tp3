739abd4c9ac9481a9bd67e17d66948308ae43ed8
gflan <twety.vg@gmail.com>
2016-07-03 22:23:50 -0300

    A ver si elimina

---
+++ tp3.h
@@ -0,0 +1,17 @@
+
+#ifndef TP3_INCLUDED_H
+#define TP3_INCLUDED_H
+
+
+#define Registro tp3::diccString<tp3::Dato>
+#include "./tp3-base/aed2/Conj.h"
+#include "./tp3-base/aed2/Lista.h"
+#include "Pila.h"
+#include "Dato.h"
+#include "diccNat.h"
+#include "diccString.h"
+#include "Tabla.h"
+#include "BaseDeDatos.h"
+
+
+#endif //TP3_INCLUDED_H
